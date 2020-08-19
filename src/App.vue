6cc98a2b4cf2cf420d39788750188e06ed4710f5
parukoo <hrk.0u0x@gmail.com>
2020-08-19 15:44:09 +0900

    マークアップ

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container my-5">
+      <div class="columns">
+        <div class="column is-4 is-offset-4">
+          <h1 class="mb-3 title">Todoリスト</h1>
+          <div class="control">
+            <label class="radio">
+              <input type="radio" name="status" checked value="all">
+              全て
+            </label>
+            <label class="radio">
+              <input type="radio" name="status" value="working">
+              作業中
+            </label>
+            <label class="radio">
+              <input type="radio" name="status" value="completed">
+              完了
+            </label>
+          </div>
+
+          <table class="table is-fullwidth">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>コメント</th>
+                <th>状態</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>b</td>
+                <td><button type="button" class="ml-2 button is-small is-light">作業中</button><button type="button" class="ml-2 button is-small is-light">削除</button></td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>b</td>
+                <td><button type="button" class="ml-2 button is-small is-light">作業中</button><button type="button" class="ml-2 button is-small is-light">削除</button></td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>b</td>
+                <td><button type="button" class="ml-2 button is-small is-light">作業中</button><button type="button" class="ml-2 button is-small is-light">削除</button></td>
+              </tr>
+            </tbody>
+          </table>
+          <h2 class="title is-4">新規タスクの追加</h2>
+          <div class="columns">
+            <input type="text" class="input">
+            <button type="button" class="ml-2 button is-dark">追加</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '../node_modules/bulma/bulma.sass';
 </style>
