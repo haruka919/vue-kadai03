fe8930e6b4447992b3223fdef5749bf0ddcbfe21
parukoo <hrk.0u0x@gmail.com>
2020-08-28 15:58:25 +0900

    表示変更をする箇所をコンポーネント化

---
+++ src/components/TodoController.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="control">
+    <label class="radio">
+      <input type="radio" name="visibility" value="all">
+      全て
+    </label>
+    <label class="radio">
+      <input type="radio" name="visibility" value="working">
+      作業中
+    </label>
+    <label class="radio">
+      <input type="radio" name="visibility" value="completed">
+      完了
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoController'
+}
+</script>
